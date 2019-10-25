93e7b67b57ec938de732fe1f3c9cbe33e2e47829
supriyaraja23@gmail.com <supriyaraja23@gmail.com>
2019-10-24 22:32:34 -0400

    Added Controller and view

--- MvcProject/MvcProject/App_Start/RouteConfig.cs
+++ MvcProject/MvcProject/App_Start/RouteConfig.cs
@@ -14,9 +14,15 @@ namespace MvcProject
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Default1",
+                url: "Emp/Details",
+                defaults: new { controller = "default", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "default", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
