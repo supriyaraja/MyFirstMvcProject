83a8fb1eae12d179ee9604717e6efe53aaef30aa
supriyaraja23@gmail.com <supriyaraja23@gmail.com>
2019-10-26 16:29:20 -0400

    Pass multiple record in Getemployee method

--- MvcProject/MvcProject/Controllers/EmployeeController.cs
+++ MvcProject/MvcProject/Controllers/EmployeeController.cs
@@ -12,11 +12,29 @@ namespace MvcProject.Controllers
         // GET: Employee
         public ViewResult GetEmployee()
         {
+            List<Employee> dbobj = new List<Employee>();
+
             Employee db = new Employee();
             db.EmpID = 1;
             db.EmpName = "Supriya";
             db.EmpSalary = 270000;
-            return View(db);
+
+            Employee db1 = new Employee();
+            db1.EmpID = 2;
+            db1.EmpName = "Manoj";
+            db1.EmpSalary = 28000;
+
+
+            Employee db2= new Employee();
+            db2.EmpID = 3;
+            db2.EmpName = "Manoj";
+            db2.EmpSalary = 28000;
+
+            dbobj.Add(db);
+            dbobj.Add(db1);
+            dbobj.Add(db2);
+
+            return View(dbobj);
         }
     }
 }
