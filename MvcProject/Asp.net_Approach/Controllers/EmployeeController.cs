a2eb0a2b1ce13f8271bb2cc7ab6cb15f7bdcf0cd
supriyaraja23@gmail.com <supriyaraja23@gmail.com>
2019-10-31 23:08:36 -0400

    Edit,update and delete code committed

--- MvcProject/Asp.net_Approach/Controllers/EmployeeController.cs
+++ MvcProject/Asp.net_Approach/Controllers/EmployeeController.cs
@@ -15,5 +15,66 @@ namespace Asp.net_Approach.Controllers
         {
             return View(db.GetEmployeeDetails());
         }
+        public ActionResult EmpDetails()
+        {
+            return View(db.GetEmployeeDetails());
+        }
+        [HttpGet]        
+        public ActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Create(EmployeeModel emp)
+        {
+            int i = db.saveEmployeeDetails(emp);
+            if (i > 0)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult Edit(int? id)
+        {
+            EmployeeModel obj = db.GetEmployeeDetailsById(id);
+            return View(obj);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(EmployeeModel e)
+        {
+            int i = db.UpdateEmployeeDetailsById(e);
+            if (i > 0)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View();
+            }
+        }
+        [HttpGet]
+        public ActionResult Delete(int? id)
+        {
+            return View();
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult DeleteConform(int? id)
+        {
+            int i = db.DeleteEmployeeDetailsById(id);
+            if (i > 0)
+            {
+                return RedirectToAction("index");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
--- MvcProject/Asp.net_Approach/Models/EmployeeContext.cs
+++ MvcProject/Asp.net_Approach/Models/EmployeeContext.cs
@@ -29,5 +29,58 @@ namespace Asp.net_Approach.Models
             }
             return list;
         }
+        public int saveEmployeeDetails(EmployeeModel obj)
+        {
+            SqlCommand cmd = new SqlCommand("sp_SaveEmployeeDetails_Warriors", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            cmd.Parameters.AddWithValue("@EmpName",obj.EmpName );
+            cmd.Parameters.AddWithValue("@EmpSalary", obj.EmpSalary);
+            object i = cmd.ExecuteScalar();
+            int result = Convert.ToInt32(i);
+            con.Close();
+            return result;
+        }
+        public EmployeeModel GetEmployeeDetailsById(int? id)
+        {
+            SqlCommand cmd = new SqlCommand("sp_GetEmployeeById_Warriors", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.AddWithValue("@EmpId", id);
+            SqlDataAdapter da = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            EmployeeModel emp = new EmployeeModel();
+            foreach (DataRow dr in dt.Rows)
+            {
+                emp.EmpId = Convert.ToInt32(dr[0]);
+                emp.EmpName = Convert.ToString(dr[1]);
+                emp.EmpSalary = Convert.ToInt32(dr[2]);
+            }
+            return emp;
+        }
+        public int UpdateEmployeeDetailsById(EmployeeModel obj)
+        {
+            SqlCommand cmd = new SqlCommand("sp_UpdateEmployeeDetails_Warriors", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            cmd.Parameters.AddWithValue("@EmpId", obj.EmpId);
+            cmd.Parameters.AddWithValue("@EmpName", obj.EmpName);
+            cmd.Parameters.AddWithValue("@EmpSalary", obj.EmpSalary);
+            object i = cmd.ExecuteNonQuery();
+            int result = Convert.ToInt32(i);
+            con.Close();
+            return result;
+        }
+        public int DeleteEmployeeDetailsById(int? id)
+        {
+            SqlCommand cmd = new SqlCommand("sp_DeleteEmployeeById", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            con.Open();
+            cmd.Parameters.AddWithValue("@EmpId", id);
+            object i = cmd.ExecuteNonQuery();
+            int result = Convert.ToInt32(i);
+            con.Close();
+            return result;
+        }        
     }
 }
