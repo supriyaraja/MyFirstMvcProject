d13bb151a3cff6e3b3e3c94a9ffe5a4c1eadf29d
supriyaraja23@gmail.com <supriyaraja23@gmail.com>
2019-11-08 23:05:10 -0500

    Added Code first approach project

---
+++ MvcProject/CodeFirstApproach/Models/EmployeeModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
+using System.Web;
+
+namespace CodeFirstApproach.Models
+{
+    public class EmployeeModel
+    {
+        [Key]
+        public int EmpId { get; set; }
+        public string EmpName { get; set; }
+        public int EmpSalary { get; set; }
+    }
+}
